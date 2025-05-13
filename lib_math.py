19514a82a1d8f93bd0b33d0b5c133fc135f7e5c5
wangxuefeng <1592193221@qq.com>
2025-05-14 03:39:21 +0800

    lib math

---
+++ lib_math.py
@@ -0,0 +1,48 @@
+import math
+
+print('math.ceil(1.1): ', math.ceil(1.1))
+print('math.floor(1.1): ', math.floor(1.1))
+print('math.pow(2, 3): ', math.pow(2, 3))
+print('math.sqrt(16): ', math.sqrt(16))
+print('math.log(10, 2): ', math.log(10, 2))
+print('math.log10(10): ', math.log10(10))
+print('math.log2(10): ', math.log2(10))
+print('math.log1p(10): ', math.log1p(10))
+print('math.log2(10): ', math.log2(10))
+
+print('math.pi: ', math.pi)
+print('math.e: ', math.e)
+print('math.tau: ', math.tau)
+print('math.inf: ', math.inf)
+print('math.nan: ', math.nan)
+print('math.isinf(math.inf): ', math.isinf(math.inf))
+print('math.isnan(math.nan): ', math.isnan(math.nan))
+print('math.isfinite(math.inf): ', math.isfinite(math.inf))
+print('math.isinf(math.nan): ', math.isinf(math.nan))
+
+
+## type
+print('type(math.pi): ', type(math.pi))
+print('type(1): ', type(1))
+print('type(1.0): ', type(1.0))
+print('type(math.tau): ', type(math.tau))
+print('type(math.inf): ', type(math.inf))
+print('type(math.nan): ', type(math.nan))
+
+## 
+print('type(math.ceil(1.1)): ', type(math.ceil(1.1)))
+print('type(math.floor(1.1)): ', type(math.floor(1.1)))
+print('type(math.pow(2, 3)): ', type(math.pow(2, 3)))
+print('type(math.sqrt(16)): ', type(math.sqrt(16)))
+print('type(math.log(10, 2)): ', type(math.log(10, 2)))
+print('type(math.log10(10)): ', type(math.log10(10)))
+print('type(math.log2(10)): ', type(math.log2(10)))
+print('type(math.log1p(10)): ', type(math.log1p(10)))
+print('type(math.log2(10)): ', type(math.log2(10)))
+
+
+## 
+print('math.ceil(1.1): ', math.ceil(1.1))
+print('math.floor(1.1): ', math.floor(1.1))
+print('math.pow(2, 3): ', math.pow(2, 3))
+print('math.sqrt(16): ', math.sqrt(16))
