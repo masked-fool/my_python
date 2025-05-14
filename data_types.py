398389f92a5f3a364888cd00c57699382735f720
wangxuefeng <1592193221@qq.com>
2025-05-14 09:03:22 +0800

    chore: date types

---
+++ data_types.py
@@ -0,0 +1,36 @@
+
+none = None
+int = 1
+float = 1.0
+complex = 1 + 2j
+str = 'hello'
+bool = True
+list = [1, 2, 3]
+tuple = (1, 2, 3)
+dict = {'a': 1, 'b': 2, 'c': 3}
+set = {1, 2, 3}
+
+
+print('type(none): ', type(none))
+print('type(int): ', type(int))
+print('type(float): ', type(float))
+print('type(complex): ', type(complex))
+print('type(str): ', type(str))
+print('type(bool): ', type(bool))
+print('type(list): ', type(list))
+print('type(tuple): ', type(tuple))
+print('type(dict): ', type(dict))
+print('type(set): ', type(set))
+
+# get length of string
+print('len(str): ', len(str))
+
+# list start from 0
+print('list[0]: ', list[0])
+print('list[1]: ', list[1])
+print('list[2]: ', list[2])
+
+# tuple start from 0
+print('tuple[0]: ', tuple[0])
+print('tuple[1]: ', tuple[1])
+print('tuple[2]: ', tuple[2])
